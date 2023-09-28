1bceaa20fb31a37c92a5948d5523f7429b340a9f
semilore <semilore@omotade-michaels.com>
2023-09-28 16:38:18 +0100

    add: part 1.6 unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const Stats = ({ type, value }) => {
+  return (
+    <div>
+      {type} {value}
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>statistics</h2>
+
+      <Stats type="good" value={good} />
+      <Stats type="neutral" value={neutral} />
+      <Stats type="bad" value={bad} />
+    </>
+  );
+};
+
+export default App;
